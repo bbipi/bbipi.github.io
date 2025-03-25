46a02d522119f79d7837cb72d1c2906ed31d9c7c
bbipi1230 <bb84832@uga.edu>
2025-03-25 12:40:44 -0400

    added resume and worked on about me page

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ window.addEventListener("scroll", function () {
         header.classList.remove("scrolled");
     }
 });
+
+// document.querySelectorAll('nav ul li a').forEach(anchor => {
+//     anchor.addEventListener('click', function(e) {
+//         if (this.getAttribute('href').startsWith('')) {
+//             e.preventDefault();
+//             const target = document.querySelector(this.getAttribute('href'));
+//             if (target) {
+//                 target.scrollIntoiView({ behavior: "smooth"});
+//             }
+//         }
+//     });
+// });
+
+function popEffect(element) {
+    element.classList.add("pop");
+
+    setTimeout(() => {
+        element.classList.remove("pop");
+    }, 200);
+}
